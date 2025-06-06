3e4eb0a461689b25efb7487976cbe6ae89de8552
Kimmo <kkpsiren@gmail.com>
2025-06-06 10:21:38 +0300

    init

---
+++ src/context/index.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { projectId, networks } from "@/config";
+import { createAppKit } from "@reown/appkit/react";
+import React, { type ReactNode } from "react";
+
+if (!projectId) {
+  throw new Error("Project ID is not defined");
+}
+
+// Set up metadata
+const metadata = {
+  name: "Send.Token",
+  description: "next-reown-appkit",
+  url: "https://github.com/0xonerb/next-reown-appkit-ssr", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+// Create the modal
+export const modal = createAppKit({
+  adapters: [],
+  projectId,
+  networks,
+  metadata,
+  themeMode: "light",
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+    socials: [],
+    email: false,
+    swaps: false,
+    onramp: false,
+    send: false,
+  },
+  themeVariables: {
+    "--w3m-accent": "#000000",
+  },
+});
+
+function ContextProvider({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
+
+export default ContextProvider;
